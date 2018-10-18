2171c1def7b971f1dfeed2204bbcf37b64b1adf0
Ian Middleton <ian.middleton.scl@gmail.com>
2018-10-18 15:46:01 -0400

    Find Integration Test
    Added integration test to ensure items inserted using the dispatch driver/reducer could successfully be accessed using the access driver "find" selector.

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -1,3 +1,4 @@
+import accessDriver from '../src/accessDriver';
 import dispatchDriver from '../src/dispatchDriver';
 import reducer from '../src/reducer';
 import ReduxObject from '../src/ReduxObject';
@@ -14,4 +15,27 @@ test('insertOne action created by dispatch driver is processed by reducer', () =
 
   // Then
   expect(updatedState[TestObject.stateSlice][testObject.id]).toBe(testObject);
+});
+
+test('inserted objects using insertOne action can be located with access driver find selector', () => {
+  // Given
+  class TestObject extends ReduxObject {}
+
+  const testObject1 = new TestObject();
+  const testObject2 = new TestObject();
+
+  let state = {};
+
+  // When
+  const action1 = dispatchDriver.insertOne(testObject1);
+  state = reducer(state, action1);
+
+  const action2 = dispatchDriver.insertOne(testObject2);
+  state = reducer(state, action2);
+
+  const selector = accessDriver.find(TestObject);
+  const locatedObjects = selector(state);
+
+  // Then
+  expect(locatedObjects).toEqual([testObject1, testObject2]);
 });
