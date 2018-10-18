1fb9db72020d25fd58014ed75dc3cc344b0f0433
Ian Middleton <ian.middleton.scl@gmail.com>
2018-10-18 15:28:10 -0400

    Basic Access Driver w/ Find
    #7 Added basic access driver with a find function. To start, I just have find getting all objects of type in the state.

---
+++ __tests__/accessDriver.test.js
@@ -0,0 +1,45 @@
+import accessDriver from '../src/accessDriver';
+import ReduxObject from '../src/ReduxObject';
+
+describe('find', () => {
+  test('if object type does not extend ReduxObject throws error', () => {
+    expect(() => accessDriver.find({})).toThrowError(
+      'objectType must extend ReduxObject.',
+    );
+  });
+
+  describe('if object type extends ReduxObject returns selector', () => {
+    test('which yields empty list if state slice for object does not exist', () => {
+      // Given
+      class TestObject extends ReduxObject {}
+
+      // When
+      const selector = accessDriver.find(TestObject);
+      const result = selector({});
+
+      // Then
+      expect(result).toEqual([]);
+    });
+
+    test('which gets all objects of type', () => {
+      // Given
+      class TestObject extends ReduxObject {}
+
+      const testObject1 = new TestObject();
+      const testObject2 = new TestObject();
+      const state = {
+        [TestObject.stateSlice]: {
+          [testObject1.id]: testObject1,
+          [testObject2.id]: testObject2,
+        },
+      };
+
+      // When
+      const selector = accessDriver.find(TestObject);
+      const result = selector(state);
+
+      // Then
+      expect(result).toEqual([testObject1, testObject2]);
+    });
+  });
+});
---
+++ src/accessDriver.js
@@ -0,0 +1,21 @@
+import { createSelector } from 'reselect';
+import ReduxObject from './ReduxObject';
+
+const allValuesSelector = slice => (slice ? Object.values(slice) : []);
+
+function createSliceSelector(stateSlice) {
+  return state => (state[stateSlice] ? state[stateSlice] : {});
+}
+
+const accessDriver = {
+  find(objectType) {
+    if (!(objectType.prototype instanceof ReduxObject)) {
+      throw new Error('objectType must extend ReduxObject.');
+    }
+
+    const sliceSelector = createSliceSelector(objectType.stateSlice);
+    return createSelector(sliceSelector, allValuesSelector);
+  },
+};
+
+export default accessDriver;
