181e581dfab5d948e244e65e7ce7b8c8fb432673
Ian Middleton <ian.middleton.scl@gmail.com>
2018-10-22 17:41:11 -0400

    Remove Errors From DispatchDriver
    #19 The errors in DispatchDriver will be handled in the reducer now as warnings, so they have been removed.

--- __tests__/DispatchDriver.test.js
+++ __tests__/DispatchDriver.test.js
@@ -4,149 +4,77 @@ import {
   DRIVER_INSERT_MANY,
   DRIVER_DELETE_ONE,
 } from '../src/actionTypes';
-import ReduxObject from '../src/ReduxObject';
 
 describe('insertOne', () => {
-  test('given object that is not an instance of a ReduxObject throws error', () => {
-    expect(() => DispatchDriver.insertOne({})).toThrowError(
-      `insertOne only accepts objects which are an instance of ${
-        ReduxObject.name
-      }.`,
-    );
+  test('returns DRIVER_INSERT_ONE action', () => {
+    // When
+    const result = DispatchDriver.insertOne();
+
+    // Then
+    expect(result.type).toBe(DRIVER_INSERT_ONE);
   });
 
-  describe('given object which is an instance of a ReduxObject', () => {
-    test('returns DRIVER_INSERT_ONE action', () => {
-      // Given
-      class TestObject extends ReduxObject {}
+  test('returns action with given object as payload', () => {
+    // Given
+    const item = {};
 
-      const testObject = new TestObject();
+    // When
+    const result = DispatchDriver.insertOne(item);
 
-      // When
-      const result = DispatchDriver.insertOne(testObject);
-
-      // Then
-      expect(result.type).toBe(DRIVER_INSERT_ONE);
-    });
-
-    test('returns action with given object as payload', () => {
-      // Given
-      class TestObject extends ReduxObject {}
-
-      const testObject = new TestObject();
-
-      // When
-      const result = DispatchDriver.insertOne(testObject);
-
-      // Then
-      expect(result.payload).toBe(testObject);
-    });
+    // Then
+    expect(result.payload).toBe(item);
   });
 });
 
 describe('insertMany', () => {
-  test('given object that is not an array throws error', () => {
-    expect(() => DispatchDriver.insertMany({})).toThrowError(
-      `insertMany only accepts arrays of ${ReduxObject.name}s.`,
-    );
-  });
-
-  test('given array containing items that are not instances of ReduxObject', () => {
-    // Given
-    class TestObject extends ReduxObject {}
-
-    const testObject1 = new TestObject();
-    const testObject2 = new TestObject();
+  test('returns DRIVER_INSERT_MANY action', () => {
+    // When
+    const result = DispatchDriver.insertMany();
 
     // Then
-    expect(() => DispatchDriver.insertMany([testObject1, {}, testObject2])).toThrowError(`insertMany only accepts arrays of ${ReduxObject.name}s.`);
+    expect(result.type).toBe(DRIVER_INSERT_MANY);
   });
 
-  describe('given object is an array of ReduxObject objects', () => {
-    test('returns DRIVER_INSERT_MANY action', () => {
-      // Given
-      class TestObject extends ReduxObject {}
+  test('returns action with given items as payload', () => {
+    // Given
+    const items = [{}, {}];
 
-      const testObject1 = new TestObject();
-      const testObject2 = new TestObject();
+    // When
+    const result = DispatchDriver.insertMany(items);
 
-      // When
-      const result = DispatchDriver.insertMany([testObject1, testObject2]);
-
-      // Then
-      expect(result.type).toBe(DRIVER_INSERT_MANY);
-    });
-
-    test('returns action with given array of ReduxObject objects as payload', () => {
-      // Given
-      class TestObject extends ReduxObject {}
-
-      const testObject1 = new TestObject();
-      const testObject2 = new TestObject();
-      const objectArray = [testObject1, testObject2];
-
-      // When
-      const result = DispatchDriver.insertMany(objectArray);
-
-      // Then
-      expect(result.payload).toBe(objectArray);
-    });
+    // Then
+    expect(result.payload).toBe(items);
   });
 });
 
 describe('deleteOne', () => {
-  test('given undefined objectType, throws error', () => {
-    expect(() => DispatchDriver.deleteOne()).toThrowError(
-      `deleteOne only accepts object types which extend ${ReduxObject.name}.`,
-    );
+  test('returns action with type of DRIVER_DELETE_ONE', () => {
+    // When
+    const result = DispatchDriver.deleteOne();
+
+    // Then
+    expect(result.type).toBe(DRIVER_DELETE_ONE);
   });
 
-  test('given null objectType, throws error', () => {
-    expect(() => DispatchDriver.deleteOne(null)).toThrowError(
-      `deleteOne only accepts object types which extend ${ReduxObject.name}.`,
-    );
+  test('returns action with payload containing given object type', () => {
+    // Given
+    const objectType = {};
+
+    // When
+    const result = DispatchDriver.deleteOne(objectType);
+
+    // Then
+    expect(result.payload.objectType).toBe(objectType);
   });
 
-  test('given objectType which does not extend ReduxObject, throws error', () => {
-    expect(() => DispatchDriver.deleteOne({})).toThrowError(
-      `deleteOne only accepts object types which extend ${ReduxObject.name}.`,
-    );
-  });
+  test('returns action with payload containing given filter', () => {
+    // Given
+    const filter = {};
 
-  describe('given objectType which does extend ReduxObject', () => {
-    test('returns action with type of DRIVER_DELETE_ONE', () => {
-      // Given
-      class TestObject extends ReduxObject {}
+    // When
+    const result = DispatchDriver.deleteOne({}, filter);
 
-      // When
-      const result = DispatchDriver.deleteOne(TestObject);
-
-      // Then
-      expect(result.type).toBe(DRIVER_DELETE_ONE);
-    });
-
-    test('returns action with payload containing given object type', () => {
-      // Given
-      class TestObject extends ReduxObject {}
-
-      // When
-      const result = DispatchDriver.deleteOne(TestObject);
-
-      // Then
-      expect(result.payload.objectType).toBe(TestObject);
-    });
-
-    test('returns action with payload containing given filter', () => {
-      // Given
-      class TestObject extends ReduxObject {}
-
-      const filter = {};
-
-      // When
-      const result = DispatchDriver.deleteOne(TestObject, filter);
-
-      // Then
-      expect(result.payload.filter).toBe(filter);
-    });
+    // Then
+    expect(result.payload.filter).toBe(filter);
   });
 });
--- src/DispatchDriver.js
+++ src/DispatchDriver.js
@@ -3,19 +3,9 @@ import {
   DRIVER_INSERT_MANY,
   DRIVER_DELETE_ONE,
 } from './actionTypes';
-import ReduxObject from './ReduxObject';
-import isReduxObjectType from './utils/isReduxObjectType';
 
 const DispatchDriver = {
   insertOne(item) {
-    if (!(item instanceof ReduxObject)) {
-      throw new Error(
-        `insertOne only accepts objects which are an instance of ${
-          ReduxObject.name
-        }.`,
-      );
-    }
-
     return {
       type: DRIVER_INSERT_ONE,
       payload: item,
@@ -23,12 +13,6 @@ const DispatchDriver = {
   },
 
   insertMany(items) {
-    if (!Array.isArray(items) || items.some(i => !(i instanceof ReduxObject))) {
-      throw new Error(
-        `insertMany only accepts arrays of ${ReduxObject.name}s.`,
-      );
-    }
-
     return {
       type: DRIVER_INSERT_MANY,
       payload: items,
@@ -36,12 +20,6 @@ const DispatchDriver = {
   },
 
   deleteOne(objectType, filter) {
-    if (!isReduxObjectType(objectType)) {
-      throw new Error(
-        `deleteOne only accepts object types which extend ${ReduxObject.name}.`,
-      );
-    }
-
     return {
       type: DRIVER_DELETE_ONE,
       payload: { objectType, filter },
