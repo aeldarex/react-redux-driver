939f9e51dcb1b362cfe29d117513f1d14c66cdd8
Ian Middleton <ian.middleton.scl@gmail.com>
2018-11-07 22:43:11 -0500

    Added Babel Transpilation
    #48 Code is now correctly transpiled using babel before publishing to npm and we only publish the essentials (aka the source, the transpiled code, and then the npm auto-includes).

---
+++ src/index.js
@@ -0,0 +1,11 @@
+export {
+  insertOne,
+  insertMany,
+  updateOne,
+  updateMany,
+  deleteOne,
+  deleteMany,
+} from './driverActions';
+export { default as driverReducer } from './reducer';
+export { default as ReduxObject } from './ReduxObject';
+export { findOne, findMany } from './selectors';
